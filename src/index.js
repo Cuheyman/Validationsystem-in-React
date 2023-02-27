8c9b6b8d293fc1d0b35a56506333f083ad882ec8
Cuheyman Kaya <cuheymankaya@live.dk>
2023-02-27 12:53:07 +0100

    Updated version

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, {Suspense} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+
+ReactDOM.render(
+<>
+    <App />
+    </>                                           
+  ,document.getElementById('root')
+);
